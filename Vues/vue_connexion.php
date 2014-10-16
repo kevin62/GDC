6c0dde39035da3dbd32124bf288e72d8c4880f50
VanpepAlexis <vanpepal@laposte.net>
2014-10-16 11:00:31 +0200

    modif mise en page

--- Vues/vue_connexion.php
+++ Vues/vue_connexion.php
@@ -2,36 +2,34 @@
 <?php
 
 	if(isset($_SESSION["Loglogin"]))
-		{
-		echo "<script langage=javascript>	alert ('tu es déjà connecté') ; </script>" ; 
+	{
 		echo "<p align=center> <a href='index.php'><font color=#ff0000> Retour a l'index  </font></a></p>" ;
-		}
-		else 
-		{
+	}
+	else 
+	{
 		echo 	
-		"<br><br><font color=#ffffff><center> Ici vous pouvez vous connecter !!! </center></font>
+		"<br><center> 
 		<div id='login'><h1><a title='connexion'></h1>
-		<table border=2><td>
-		<table>
-		<td>
-		<img  class=image src='./Images/conn.jpg' width=200 height=200>
-		</td><td valign=top ><form action='index.php' method='POST' >
-			<p>
-				<label><font color=#ffffff>Identifiant</font><br>
-				<input type='text' tabindex='10' size='20'   id='user_login' name='log'></label>
-			</p>
-			<p>
-				<label><font color=#ffffff>Mot de passe</font><br>
-				<input type='password' tabindex='20' size='20'  id='user_pass' name='pwd'></label>
-		<br><br><center><input type='submit' id='envoie'/></center></td></table></td></table>
-		</form>
+			<table border=2><td>
+			<table>
+				<p>
+					<br><font face='Helvetica' color='red'><b><center><label>Identifiant<br>
+					<input type='text' tabindex='10' size='20'   id='user_login' name='log'></label>
+				</p>
+				<p>
+					<label>Mot de passe<br>
+					<input type='password' tabindex='20' size='20'  id='user_pass' name='pwd'></label>
+				</p>
+				<center><input type='submit' id='envoie'/></center><br>
+			</table>
+			</td></table>
 		</div>
-
+		
+		
+		<br>
 		<div id='Oops'>
-		<font color=#ffffff> si tu n'es pas encore inscris ce n'est pas la que tu dois etre!! regarde ici =><a href='index.php?vue=vue_inscript.php'><font color=#ff0000>  INSCRIPTION </font></a>
-		sinon tu peux toujours retourner sur l'acceuil ici => <a href='index.php'><font color=#ff0000> ACCUEIL</font></a></font>
+			<a href='index.php?vue=vue_inscript.php'> INSCRIPTION </font></a>
 		</div>
-
-		</html> " ;
-		}
+		</center>" ;
+	}
 ?>
